dba16af0ab480b73486dee66606c5432d92960be
Alexandre tuet <tuet.alex@gmail.com>
2019-07-07 14:59:57 +0200

    added: schema for token
    Co-authored-by: Arthur 'Woosy' <arthur.dufour@epsi.fr>

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const tokenSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   value: { type: String, required: true },
-  type: { type: String, enum: [''] }
+  type: { type: String, enum: ['EMAIL_VERIFICATION', 'PASSWORD_VERIFICATION'] }
 })
 
 module.exports = mongoose.model('Token', tokenSchema)
