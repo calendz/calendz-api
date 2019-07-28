1f9fc7a2f0d82437108d95dc53f95f93b6e94238
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-07-28 21:27:39 +0200

    test(services): add tests

---
+++ src/tests/services/jwt.service.spec.js
@@ -0,0 +1,34 @@
+const assert = require('chai').assert
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const config = require('../../config/config')
+const JwtService = require('../../services/jwt.service')
+
+describe('./services/jwt.service', () => {
+  const user = {
+    userId: mongoose.Types.ObjectId(),
+    firstname: 'John',
+    lastname: 'Doe',
+    email: 'john.doe@epsi.fr',
+    permissionLevel: 'MEMBER'
+  }
+
+  // ===============================================
+  // == Methods
+  // ===============================================
+  describe('#create', () => {
+    it('should create a token with rememberMe', (done) => {
+      JwtService.create(user, true).then((token) => {
+        jwt.verify(token, config.jwt.secret)
+        done()
+      })
+    })
+
+    it('should create a token without rememberMe', (done) => {
+      JwtService.create(user, false).then((token) => {
+        jwt.verify(token, config.jwt.secret)
+        done()
+      })
+    })
+  })
+})
