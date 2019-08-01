f35da270f00aa08765b5b5c86c0eb318541a7c18
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-08-01 19:28:44 +0200

    fix(cookie): config maxAge

--- src/config/cookie.js
+++ src/config/cookie.js
@@ -3,7 +3,7 @@ const config = require('./config')
 const cookieConfig = {
   httpOnly: true,
   secure: config.node_env !== 'development',
-  maxAge: config.jwt.expiration_refresh * 60 * 3600 * 24,
+  maxAge: config.jwt.expiration_refresh * 1000 * 3600 * 24,
   signed: true
 }
 
