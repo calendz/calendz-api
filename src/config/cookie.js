57810a3bdbb63bcd57fdd5237f5f9a9607df18ce
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-08-02 17:08:54 +0200

    feat(config): add config for accessToken & refreshToken

--- src/config/cookie.js
+++ src/config/cookie.js
@@ -1,17 +1,15 @@
 const config = require('./config')
 
-// const cookieConfig = {
-//   httpOnly: true,
-//   secure: config.node_env !== 'development',
-//   maxAge: config.jwt.expiration_refresh * 1000 * 3600 * 24,
-//   signed: true
-// }
-
-const cookieConfig = {
+exports.accessTokenConfig = {
   httpOnly: true,
   secure: false,
-  signed: false,
-  maxAge: config.jwt.expiration_refresh * 1000 * 3600 * 24
+  signed: true,
+  maxAge: config.jwt.expiration * 1000
 }
 
-module.exports = cookieConfig
+exports.refreshTokenConfig = {
+  httpOnly: true,
+  secure: false,
+  signed: true,
+  maxAge: config.jwt.expiration_refresh * 1000 * 3600 * 24
+}
