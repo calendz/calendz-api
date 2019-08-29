d6f278430d0e87ae2ee8167b171ed85e1708b88f
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-08-29 10:51:47 +0200

    feat(get-version): add GET /version route

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,11 +2,14 @@ const express = require('express')
 const authRoutes = require('./auth.route')
 const userRoutes = require('./user.route')
 const notificationsRoutes = require('./notifications.route')
+const version = require('../../package.json').version
 
 const router = express.Router()
 
 // Route utilisée pour vérifier si l'API est up
 router.get('/health-check', (req, res) => { res.send('OK') })
+// Route utilisée pour récupérer la version de l'api
+router.get('/version', (req, res) => { res.json({ version }) })
 
 router.use('/auth', authRoutes)
 router.use('/user', userRoutes)
