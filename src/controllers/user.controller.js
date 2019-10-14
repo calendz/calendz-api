3c234e3071b7eada8b19414544b863487f07c8be
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-10-14 19:10:38 +0200

    feat(user-routes): refactored "register" & "update user" routes with user's "group" property

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,10 +21,11 @@ exports.create = async (req, res) => {
   const _email = req.body.email
   const _password = req.body.password
   const _grade = req.body.grade
+  const _group = req.body.group
   const _city = req.body.city
 
   // création utilisateur et création token confirmation mail
-  const user = await UserService.create(_firstname, _lastname, _email, _password, _grade, _city)
+  const user = await UserService.create(_firstname, _lastname, _email, _password, _grade, _group, _city)
   const token = await TokenService.create(user._id, uuidv4(), 'EMAIL_VERIFICATION')
 
   // envoie mail de confirmation
@@ -111,11 +112,12 @@ exports.updateUserInformations = async (req, res) => {
   const _email = req.body.email
   const _permissionLevel = req.body.permissionLevel
   const _grade = req.body.grade
+  const _group = req.body.group
   const _city = req.body.city
   const _bts = req.body.bts
   const _isActive = req.body.isActive
 
-  await UserService.updateUserInformations(_userId, _firstname, _lastname, _email, _permissionLevel, _grade, _city, _bts, _isActive)
+  await UserService.updateUserInformations(_userId, _firstname, _lastname, _email, _permissionLevel, _grade, _group, _city, _bts, _isActive)
 
   return res.status(200).json({
     message: 'Les informations ont bien été modifiées'
