755a07da234946e6af6d1e72ed7945bedec17399
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-11-02 11:56:55 +0100

    refactor(task-model): allow null for city, grade & group

--- src/mock/factories/task.factory.js
+++ src/mock/factories/task.factory.js
@@ -7,11 +7,11 @@ module.exports = class Task {
     date = new Date(faker.date.recent(-30)).getTime(),
     type = faker.random.arrayElement(['DS', 'homework', 'task']),
     title = faker.lorem.lines(1),
-    description = faker.lorem.lines(2),
-    subject = faker.random.words(2),
-    city = faker.random.arrayElement(['Arras', 'Auxerre', 'Bordeaux', 'Brest', 'Grenoble', 'Lille', 'Lyon', 'Montpellier', 'Nantes', 'Paris', 'Dakar']),
-    grade = faker.random.arrayElement(['B1', 'B2', 'B3', 'I1', 'I2']),
-    group = faker.random.arrayElement(['G1', 'G2', 'G3', 'G1 (dev)', 'G2 (dev)', 'G3 (dev)', 'G1 (infra-réseau)', 'G2 (infra-réseau)', 'G3 (infra-réseau)']),
+    description = null,
+    subject = '',
+    city = null,
+    grade = null,
+    group = null,
     targets = []
   }) {
     this.author = author
--- src/models/task.model.js
+++ src/models/task.model.js
@@ -10,9 +10,9 @@ const taskSchema = new Schema({
   // not required
   description: { type: String, minlength: 2, maxlength: 1000, required: false },
   subject: { type: String, minLength: 2, maxlength: 50, required: false },
-  city: { type: String, enum: ['Arras', 'Auxerre', 'Bordeaux', 'Brest', 'Grenoble', 'Lille', 'Lyon', 'Montpellier', 'Nantes', 'Paris', 'Dakar'], required: false },
-  grade: { type: String, enum: ['B1', 'B2', 'B3', 'I1', 'I2'], required: false },
-  group: { type: String, enum: ['G1', 'G2', 'G3', 'G1 (dev)', 'G2 (dev)', 'G3 (dev)', 'G1 (infra-réseau)', 'G2 (infra-réseau)', 'G3 (infra-réseau)'], required: false },
+  city: { type: String, enum: [null, 'Arras', 'Auxerre', 'Bordeaux', 'Brest', 'Grenoble', 'Lille', 'Lyon', 'Montpellier', 'Nantes', 'Paris', 'Dakar'], required: false },
+  grade: { type: String, enum: [null, 'B1', 'B2', 'B3', 'I1', 'I2'], required: false },
+  group: { type: String, enum: [null, 'G1', 'G2', 'G3', 'G1 (dev)', 'G2 (dev)', 'G3 (dev)', 'G1 (infra-réseau)', 'G2 (infra-réseau)', 'G3 (infra-réseau)'], required: false },
   targets: [{ type: Schema.Types.ObjectId, ref: 'User', required: false }]
 })
 
