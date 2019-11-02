083680403cce79acf480698665a1466903a44b11
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-11-02 21:20:04 +0100

    test(tasks): added tests for "post /tasks" route

--- src/tests/routes/tasks.route.spec.js
+++ src/tests/routes/tasks.route.spec.js
@@ -7,9 +7,9 @@ const authHelper = require('../helpers/auth.helper')
 
 describe('./routes/tasks.route', () => {
   // ===================================================================
-  // == GET /v1/notifications/:userId - get user's notifications
+  // == GET /v1/tasks/:userId - get user's tasks
   // ===================================================================
-  describe(`GET /v1/notifications/:userId - get user's notifications`, () => {
+  describe(`GET /v1/tasks/:userId - get user's tasks`, () => {
     authHelper.requireAuth('get', '/v1/tasks/5d4f26aa046ad506f9583bd3')
     authHelper.requireAdminOrSameUser('get', '/v1/tasks/5d4f26aa046ad506f9583bd3')
 
@@ -40,6 +40,150 @@ describe('./routes/tasks.route', () => {
           if (err) return done(err)
           assert.isDefined(res.body.tasks)
           assert.isArray(res.body.tasks)
+          done()
+        })
+    })
+  })
+
+  // ==========================================================
+  // == POST /v1/tasks - create a task
+  // ==========================================================
+  describe(`POST /v1/tasks - create a task`, () => {
+    const date = '02-11-2019'
+    const type = 'task'
+    const title = 'Un titre de tâche'
+    const description = 'Description de ma tâche de test'
+    const subject = 'Une matière'
+
+    authHelper.requireAuth('post', '/v1/tasks', { title, type, subject, date, description })
+
+    it('should fail (412) : missing title', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ type, subject, date, description })
+        .expect(412)
+        .end((err, res) => {
+          if (err) return done(err)
+          helper.hasBodyMessage(res.body, 'Certains champs requis sont manquant')
+          helper.hasBodyErrorsThatContains(res.body, 'Veuillez indiquer un titre')
+          done()
+        })
+    })
+
+    it('should fail (412) : missing type', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ title, subject, date, description })
+        .expect(412)
+        .end((err, res) => {
+          if (err) return done(err)
+          helper.hasBodyMessage(res.body, 'Certains champs requis sont manquant')
+          helper.hasBodyErrorsThatContains(res.body, 'Veuillez indiquer un type')
+          done()
+        })
+    })
+
+    it('should fail (412) : missing date', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ title, subject, type, description })
+        .expect(412)
+        .end((err, res) => {
+          if (err) return done(err)
+          helper.hasBodyMessage(res.body, 'Certains champs requis sont manquant')
+          helper.hasBodyErrorsThatContains(res.body, 'Veuillez indiquer une date')
+          done()
+        })
+    })
+
+    it('should fail (412) : title too short', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ title: 'a', subject, type, date, description })
+        .expect(412)
+        .end((err, res) => {
+          if (err) return done(err)
+          helper.hasBodyMessage(res.body, 'Certains champs requis sont invalides')
+          helper.hasBodyErrorsThatContains(res.body, 'Le titre indiqué est trop court')
+          done()
+        })
+    })
+
+    it('should fail (412) : title too long', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ title: 'azeazeaeaeaeaeaeaeaeaeazeazeaeaeyauieyaiuehaiehazoi', subject, type, date, description })
+        .expect(412)
+        .end((err, res) => {
+          if (err) return done(err)
+          helper.hasBodyMessage(res.body, 'Certains champs requis sont invalides')
+          helper.hasBodyErrorsThatContains(res.body, 'Le titre indiqué est trop long')
+          done()
+        })
+    })
+
+    it('should fail (412) : subject too short', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ title, subject: 'a', type, date, description })
+        .expect(412)
+        .end((err, res) => {
+          if (err) return done(err)
+          helper.hasBodyMessage(res.body, 'Certains champs requis sont invalides')
+          helper.hasBodyErrorsThatContains(res.body, 'La matière indiquée est trop courte')
+          done()
+        })
+    })
+
+    it('should fail (412) : subject too long', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ title, subject: 'azeazeaeaeaeaeaeaeaeaeazeazeaeaeyauieyaiuehaiehazoi', type, date, description })
+        .expect(412)
+        .end((err, res) => {
+          if (err) return done(err)
+          helper.hasBodyMessage(res.body, 'Certains champs requis sont invalides')
+          helper.hasBodyErrorsThatContains(res.body, 'La matière indiquée est trop longue')
+          done()
+        })
+    })
+
+    it('should fail (412) : description too short', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ title, subject, type, date, description: 'a' })
+        .expect(412)
+        .end((err, res) => {
+          if (err) return done(err)
+          helper.hasBodyMessage(res.body, 'Certains champs requis sont invalides')
+          helper.hasBodyErrorsThatContains(res.body, 'La description indiquée est trop courte')
+          done()
+        })
+    })
+
+    it('should fail (412) : invalid date', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ title, subject, type, date: 'someInvalidDate', description })
+        .expect(412)
+        .end((err, res) => {
+          if (err) return done(err)
+          helper.hasBodyMessage(res.body, 'Certains champs requis sont invalides')
+          helper.hasBodyErrorsThatContains(res.body, 'La date indiquée est invalide')
+          done()
+        })
+    })
+
+    it('should fail (412) : invalid type', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ title, subject, type: 'someInvalidType', date, description })
+        .expect(412)
+        .end((err, res) => {
+          if (err) return done(err)
+          helper.hasBodyMessage(res.body, 'Certains champs requis sont invalides')
+          helper.hasBodyErrorsThatContains(res.body, 'Le type indiqué est invalide')
+          done()
+        })
+    })
+
+    it('should fail (200) : invalid type', (done) => {
+      request(app).post('/v1/tasks').set(helper.defaultSetsWithAccess).expect('Content-Type', /json/)
+        .send({ title, subject, type, date, description })
+        .expect(201)
+        .end((err, res) => {
+          if (err) return done(err)
+          assert.isDefined(res.body.task)
           done()
         })
     })
--- src/tests/services/task.service.spec.js
+++ src/tests/services/task.service.spec.js
@@ -1,8 +1,18 @@
 const assert = require('chai').assert
+const mongoose = require('mongoose')
 const TasksService = require('../../services/tasks.service')
 
 describe('./services/tasks.service', () => {
   const user = '5d4f26aa046ad506f9583bd3'
+  const date = new Date().getTime()
+  const type = 'task'
+  const title = 'Un titre de tâche'
+  const description = 'Description de ma tâche de test'
+  const subject = 'Une matière'
+  const city = 'Lyon'
+  const grade = 'B3'
+  const group = 'G1 (dev)'
+  const targets = []
 
   // ===============================================
   // == Methods
@@ -13,6 +23,25 @@ describe('./services/tasks.service', () => {
       TasksService.getAllFrom(user).then(tasks => {
         assert.isDefined(tasks)
         assert.isArray(tasks)
+        done()
+      }).catch(err => done(err))
+    })
+  })
+
+  describe('#create', () => {
+    it('should create a task', (done) => {
+      TasksService.create(user, date, type, title, description, subject, city, grade, group, targets).then(task => {
+        assert.isTrue(task._id instanceof mongoose.mongo.ObjectID)
+        assert.isTrue(task.author._id instanceof mongoose.mongo.ObjectID)
+        assert.strictEqual(task.date, date.toString())
+        assert.strictEqual(task.type, type)
+        assert.strictEqual(task.title, title)
+        assert.strictEqual(task.description, description)
+        assert.strictEqual(task.subject, subject)
+        assert.strictEqual(task.city, city)
+        assert.strictEqual(task.grade, grade)
+        assert.strictEqual(task.group, group)
+        assert.isArray(task.targets)
         done()
       }).catch(err => done(err))
     })
