ed6493f2ac0e57f97f41832e6f50255f03c930a2
Arthur Dufour <dufourarthur.perso@gmail.com>
2020-03-13 16:22:13 +0100

    fix(wis): fixed wis email format

--- src/tests/services/user.service.spec.js
+++ src/tests/services/user.service.spec.js
@@ -53,10 +53,10 @@ describe('./services/user.service', () => {
     })
 
     it('should create a user (wis)', (done) => {
-      UserService.create(firstname, lastname, 'john.doe2@etudes-wis.net', password, grade, group, city).then((user) => {
+      UserService.create(firstname, lastname, 'john.doe2@ecoles-wis.net', password, grade, group, city).then((user) => {
         assert.strictEqual(user.firstname, firstname)
         assert.strictEqual(user.lastname, lastname)
-        assert.strictEqual(user.email, 'john.doe2@etudes-wis.net')
+        assert.strictEqual(user.email, 'john.doe2@ecoles-wis.net')
         assert.strictEqual(user.school, 'WIS')
         assert.strictEqual(user.grade, grade)
         assert.strictEqual(user.group, group)
