53c22497a4732ecd483ea8c850b156a64e03abbd
Arthur Dufour <dufourarthur.perso@gmail.com>
2020-09-03 21:17:53 +0200

    fix(grade-model): coefficient isn't required (and fix date)

--- src/models/grade.model.js
+++ src/models/grade.model.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema
 const gradeSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   value: { type: Number, required: false },
-  coefficient: { type: Number, required: true, default: 1 },
+  coefficient: { type: Number, required: false, default: 1 },
   subject: { type: String, required: true, minlength: 1, maxlength: 50 },
-  date: { type: String, required: true, default: () => Date.now() },
+  date: { type: String, required: true },
   description: { type: String, required: false, minlength: 1, maxlength: 250 }
 })
 
