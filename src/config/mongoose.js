2762fd69b676cb38de99c64c82a2380ef7eb0249
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-01-26 22:39:27 +0100

    fix(Mongoose): #OIC-15 deprecated value for keepAlive

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -12,7 +12,7 @@ module.exports = async function initConnection (callback) {
   const mongoHost = config.node_env === 'test' || config.node_env === 'mock' ? 'localhost' : config.db.host
   const mongoUri = `mongodb://${config.db.user}:${config.db.password}@${mongoHost}:${config.db.port}/${config.db.name}${config.travis ? '' : '?authSource=admin'}`
   const mongooseOpt = {
-    keepAlive: 1,
+    keepAlive: true,
     useNewUrlParser: true
   }
 
